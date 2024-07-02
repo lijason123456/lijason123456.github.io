e07e0a6da7ffead6111456ed9983527822d95d39
lijason123456 <168806511+lijason123456@users.noreply.github.com>
2024-07-02 10:49:25 +0800

    Add files via upload

---
+++ index/show.js
@@ -0,0 +1,52 @@
+//获取节点
+let artEle = document.querySelector("article");
+let secEle = document.getElementsByTagName("section")[0];
+
+//创建数组
+let arr = [
+    {time: "2024-7-2", text: "lijason的主页", to:"./../"},
+    {time: "2024-7-2", text: "lijason的博客", to:"./../blog/"},
+]
+
+//将数据渲染到页面上
+function render() {
+    let str = "";
+    let count = 0
+    arr.forEach((item, index) => {
+        count += 1
+        str += `
+        <div>
+           <p>
+              <a href="${item.to}">${count}.${item.text}</a>
+              <span>${item.time}</span>
+           </p>
+        </div>
+        `;
+    });
+    secEle.innerHTML = str;
+}
+
+//给祖先元素article绑定一个事件监听器（事件代理）
+artEle.addEventListener("click", function (e) {
+    let index = e.target.dataset.index - 0;
+    if (e.target.value == "留言") {
+        let textAreaEle = document.getElementsByTagName("textarea")[0];
+
+        //获取本地时间
+        let date = new Date();
+        let localTime = date.toLocaleString();//年月日、时分秒同时获取
+        //将原本的/ 换成-
+        //replace该方法主要用于将字符串中符合匹配条件的字串替换成其他的字符串，
+        //返回替换后的字符串，且原字符串不变。
+        let gang = localTime.replace(/\//g, "-");
+        //将用户输入的内容容作为对象存进数组
+        arr.push(
+            { name: "", time: gang, content: textAreaEle.value }
+        )
+        //清空输入框，方便下次输入
+        textAreaEle.value = "";
+        //再次渲染
+        render();
+    }
+})
+render();
